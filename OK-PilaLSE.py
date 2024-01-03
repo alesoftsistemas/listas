55958f211b223fe18df7d8550e12d3ce70fbdb99
alesoftsistemas <155382769+alesoftsistemas@users.noreply.github.com>
2024-01-02 21:53:16 -0300

    Add files via upload

--- OK-PilaLSE.py
+++ OK-PilaLSE.py
@@ -12,48 +12,27 @@ class Nodo():
     self.dato = dato
     self.siguiente = siguiente
 
-class ListaLigada:
-
-  def __init__(self):
-    self.cabeza = None
-    self.tamano = 0
-
-  def unir(self, dato):
-    nodo = Nodo(dato, None)
-
-    if self.cabeza == None:
-      self.cabeza = nodo
-    else:
-      posicion_actual = self.cabeza
-
-      while posicion_actual.siguiente:
-        posicion_actual = posicion_actual.siguiente
-
-      posicion_actual.siguiente = nodo
-
-    self.tamano += 1
-
-
-# Instancia de la clase
-pila = []  
+print("PILA en Lista Simple Encadenada \n")
+pila = []  # Iniciamos la pila vacia
+print("Pila =", pila, "\n")
 
 print("Ingreso de las claves")
 print("Ingresa A")
 pila.append("A")
-print(pila, "\n")
+print("Pila =", pila, "\n")
 
 print("Ingresa B")
 pila.append("B")
-print(pila, "\n")
+print("Pila =", pila, "\n")
 
 print("Ingresa C")
 pila.append("C")
-print(pila, "\n")
+print("Pila =", pila, "\n")
 
 print("Ingresa D")
 pila.append("D")
-print(pila, "\n")
+print("Pila =", pila, "\n")
 
 print("Elimina 1 elemento")
 pila.pop()
-print(pila, "\n")
+print("Pila =",pila, "\n")
